add515a7312f73044536242f2c4649e81ea2490a
OloV03 <jukov46@yandex.ru>
2021-10-27 15:58:09 +0300

    Начальный возврат модуля Yandex

---
+++ Yandex/Network.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading.Tasks;
+
+
+namespace Yandex
+{
+    public class Network
+    {
+        public void Inputvalue(ChromeDriver chromeDriver, string value)
+        {
+
+            List<IWebElement> webElements = chromeDriver.FindElementsByClassName("input__control").ToList();
+            foreach (var item in webElements)
+            {
+                if (!item.Displayed)
+                    continue;
+                if (item.GetAttribute("placeholder") != "Поиск мест и адресов")
+                    continue;
+                item.Clear();
+                item.SendKeys(value);
+            }
+
+            Thread.Sleep(1000);
+            IWebElement button = chromeDriver.FindElementByXPath("/html/body/div[1]/div[2]/div[3]/div/div/div/div/form/div[3]/button");
+            button.Click();
+            Thread.Sleep(1000);
+
+            IWebElement webElement = chromeDriver.FindElementByClassName("toponym-card-title-view__coords-badge");
+
+            double xi = Convert.ToDouble(webElement.Text.Substring(0, 9));
+
+            double yi = Convert.ToDouble(webElement.Text.Substring(10, 10));
+
+            Console.WriteLine("Координаты (" + value + "): " + webElement.Text);
+
+            double x = 55.752692;
+            double y = 37.620902;
+
+            double xC = 55.776049; // х Севера
+            double xY = 55.729335; // х Юга
+
+            double yZ = 37.581097; // y Запада 
+            double yV = 37.660707; // y Востока
+
+            if (xi <= xC && xi >= xY)
+            {
+                if (yi <= yV && yi >= yZ) Console.WriteLine("Центр " + value);
+            }
+
+            else if (xi > xC)
+            {
+                if (yi > y) Console.WriteLine("Северо-Восток " + value);
+                else Console.WriteLine("Северо-Запад " + value);
+            }
+
+            else if (xi < xY)
+            {
+                if (yi > y) Console.WriteLine("Юго-Восток " + value);
+                else Console.WriteLine("Юго-Запад " + value);
+            }
+
+            else if (yi > yV)
+            {
+                if (xi > x) Console.WriteLine("Северо-Восток " + value);
+                else Console.WriteLine("Юго-Восток " + value);
+            }
+
+            else if (yi < yZ)
+            {
+                if (xi > x) Console.WriteLine("Северо-Запад " + value);
+                else Console.WriteLine("Юго-Запад " + value);
+            }
+
+
+            Console.WriteLine("Готово");
+        }
+
+        public void valueOnMap(ChromeDriver chrome, string[] values)
+        {
+            int i = 0;
+            chrome.Navigate().GoToUrl("https://yandex.ru/maps/213/moscow/?ll=37.612038%2C55.733991&mode=routes&rtext=&rtt=auto&z=11.88");
+            List<IWebElement> webElements = chrome.FindElementsByTagName("input").ToList();
+            List<IWebElement> webElements1 = chrome.FindElementsByTagName("span").ToList();
+
+            foreach (var item in webElements)
+            {
+                if (!item.Displayed)
+                    continue;
+                if (item.GetAttribute("class") != "input__control")
+                    continue;
+                if (item.GetAttribute("placeholder") != "Откуда")
+                    continue;
+                item.SendKeys(values[i] + "\n");
+                i++;
+            }
+
+            foreach (var item in webElements)
+            {
+                if (!item.Displayed)
+                    continue;
+                if (item.GetAttribute("class") != "input__control")
+                    continue;
+                if (item.GetAttribute("placeholder") != "Куда")
+                    continue;
+                item.SendKeys(values[i] + "\n");
+                i++;
+            }
+
+            for (int j = 0; j < values.Length - 2; j++)
+            {
+                // жмем кнопку добавить точку
+                webElements = chrome.FindElementsByClassName("route-form-view__add").ToList();
+                foreach (var item in webElements)
+                {
+                    if (!item.Displayed)
+                        continue;
+                    if (item.Text != "Добавить точку")
+                        continue;
+                    item.Click();
+                    break;
+                }
+
+                webElements = chrome.FindElementsByTagName("input").ToList();
+                foreach (var item in webElements)
+                {
+                    if (!item.Displayed)
+                        continue;
+                    if (item.GetAttribute("class") != "input__control")
+                        continue;
+                    if (item.GetAttribute("placeholder") != "Куда")
+                        continue;
+                    item.SendKeys(values[i] + "\n");
+                    i++;
+                }
+            }
+        }
+    }
+}
---
+++ Yandex/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Linq;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading.Tasks;
+
+namespace Yandex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Network net = new Network();
+            string[] value = new string[] { };
+
+            Console.Title = "Встречи Судьбы";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            EnterWord();
+
+            Console.WriteLine("Доступные команды:\n-1-: локация адреса+координаты \t-2-: расстановка адресов на Яндекс Картах");
+
+            string choice = Console.ReadLine();
+            if (choice == "1")
+            {
+                Console.WriteLine("\nПеренесите в окно консоли файл с адресами будующих встреч");
+                Console.Write("Путь к файлу: ");
+                string file = Console.ReadLine();
+                value = AddressToArray(file);
+
+                ChromeDriver chrome = new ChromeDriver();
+                Console.Clear();
+                chrome.Navigate().GoToUrl("https://yandex.ru/maps/213/moscow/?ll=37.622504%2C55.753215&z=10");
+                for (int i = 0; i < value.Length; i++)
+                {
+                    net.Inputvalue(chrome, value[i]);
+                    chrome.Navigate().Refresh();
+                }
+            }
+
+            if (choice == "2")
+            {
+                Console.WriteLine("\nПеренесите в окно консоли файл с адресами будующих встреч");
+                Console.Write("Путь к файлу: ");
+                string file = Console.ReadLine();
+                value = AddressToArray(file);
+
+                ChromeDriver chrome = new ChromeDriver(".");
+                Console.Clear();
+
+                EnterWord();
+
+                Console.WriteLine("Finished");
+                chrome.Navigate().GoToUrl("https://yandex.ru/maps/213/moscow/?ll=37.622504%2C55.753215&z=10");
+                net.valueOnMap(chrome, value);
+            }
+
+            if (choice == "3")
+            {
+                List<string> text = new List<string>();
+                while (true)
+                {
+                    string x = Console.ReadLine();
+                    if (x != "")
+                    {
+                        text.Add(x);
+                    }
+                    else break;
+                }
+                foreach (var item in text)
+                {
+
+                    Console.WriteLine(item);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static string[] AddressToArray(string values)
+        {
+            List<string> result = new List<string>();
+
+            using (StreamReader sr = new StreamReader(values))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    line = line.Trim();
+                    result.Add(line);
+                }
+            }
+
+            return result.ToArray();
+        }
+
+        static void EnterWord()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine(" Добро пожаловать в приложение \"Встречи Судьбы\"");
+            Console.WriteLine("-------------------------------------------------\n\n");
+            Console.ResetColor();
+        }
+    }
+}
